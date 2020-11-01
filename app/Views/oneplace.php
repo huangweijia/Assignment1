75ec9b544b727b51c53cbddf86f970e4a7c6e78e
Administrator <3302240100@qq.com>
2020-11-01 19:55:43 +0800

    Nightwish

--- app/Views/oneplace.php
+++ app/Views/oneplace.php
@@ -4,13 +4,12 @@
 		<title>DGPT4711 Lab 04</title>
 	</head>
 	<body>
-                <h1>Nighwish</h1>
+                <h1>overwatch</h1>
 		<p><strong>ID:</strong> {id}</p>
 		<p><strong>Name:</strong> {name}</p>
 		<p><strong>Fullname:</strong> {fullname}</p>
                 <p><strong>Position:</strong> {position}</p>
                 <p><strong>Nationality:</strong> {nationality}</p>
-                <p><strong>Birthplace:</strong> {birthplace}</p>
                 <p><strong>Profession:</strong> {profession}</p>      
 		<p><img src="/image/{image}"/></p>
 		<p><a href="/home">Home</a></p>
